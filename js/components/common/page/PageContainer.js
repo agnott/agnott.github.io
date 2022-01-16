4c050ce0dd101f30f94c86ce810e8c544696a4d8
Andrew Gnott <gnottandrew@gmail.com>
2022-01-16 16:03:15 -0500

    ref(core): use next js

--- js/components/common/page/PageContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PageContainer(props) {
-  return (
-    <div className="page__container">
-      { props.children }
-    </div>
-  );
-}
--- js/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import '../css/main.css';
-
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('viewport'));
